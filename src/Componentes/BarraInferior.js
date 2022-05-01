11a07ee13027668f5527f3fe9d5d08d041185a23
Vitor Gabriel <vittorgabriell420@gmail.com>
2022-05-01 15:34:02 -0300

    inserindo botao home
    inserir o botao home na barra inferior

--- src/Componentes/BarraInferior.js
+++ src/Componentes/BarraInferior.js
@@ -1,8 +1,11 @@
 import { SafeAreaView, StyleSheet, Text, View ,Image,TouchableOpacity} from 'react-native';
 
+import ButtonHome from './ButtonHome';
+
 export default function(){
     return(
         <View style={styles.barraInferior}>
+            <ButtonHome/>
 
         </View>
     );
@@ -11,7 +14,7 @@ export default function(){
 const styles = StyleSheet.create({
     barraInferior:{
         flexDirection:'row',
-        backgroundColor:'green',
+        backgroundColor:'#1C1C1C',
         width:'100%',
         height:'5%',
         flex:1,
--- src/Componentes/ButtonHome.js
+++ src/Componentes/ButtonHome.js
@@ -1,7 +1,30 @@
 import { SafeAreaView, StyleSheet, Text, View ,Image,TouchableOpacity} from 'react-native';
 
+
+
 export default function(){
     return(
+        <View>
+        <TouchableOpacity>
+            <Image source={require('../../assets/home-branco-48.png')}
+            style={styles.estiloBotao}/>
+            <Text style={styles.estiloLetras}>Inicio</Text>
+        </TouchableOpacity></View>
 
     );
 }
+
+const styles = StyleSheet.create({
+    estiloBotao:{
+        width:40,
+        height:40,
+        resizeMode:'contain',
+        marginLeft:5,
+        marginTop:10
+    },
+    estiloLetras:{
+        fontSize:20,
+        fontWeight:'bold',
+        color:'#fff'
+    }
+});
