07d492e1a7a7b422125617f44e91e15a3d0d3eec
Vitor Gabriel <vittorgabriell420@gmail.com>
2022-05-04 15:33:24 -0300

    teste
    testando, ainda não foi concluido

--- App.js
+++ App.js
@@ -5,19 +5,29 @@ import {WebView} from  'react-native-webview'
 import BarraSuperior from './src/Componentes/BarraSuperior';
 import BarraInferior from './src/Componentes/BarraInferior'; 
 import Home from './screens/Home';
+import Index from '.screens/Index';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Index" component={Index} />
+      <Stack.Screen name="teste" component={Notifications} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Settings" component={Settings} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-    <BarraSuperior/>
-    <View style={styles.conteudo}>
-    <Text>Reservado para o conteudo</Text>
-    </View>
-  
-
-    <BarraInferior/>
-
-    </SafeAreaView>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
 
